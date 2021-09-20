0a0422cb89ff6d5a62ab3a05b12037c65ef17125
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-09-21 03:56:43 +0530

    volunter list updated

--- components/helper/teamHelper.js
+++ components/helper/teamHelper.js
@@ -43,7 +43,27 @@ export const coreLeads = [
     }
 ]
 
+/*
+Diganta - https://firebasestorage.googleapis.com/v0/b/dscbppimt-cms-storage.appspot.com/o/member_images%2Fdiganta_ghosh_Y9EpQ81.jpeg?alt=media&token=1161a4e2-159f-464b-9c95-0f6960b9edc4
+Debjita - https://firebasestorage.googleapis.com/v0/b/dscbppimt-cms-storage.appspot.com/o/member_images%2Fdebjita_mondal.jpg?alt=media&token=6b96793a-7d90-4eb5-a49f-90e5addf89f9 
+*/
 export const coreMembers = [
+    {
+        name: "Sosha Ghosh",
+        image: "https://firebasestorage.googleapis.com/v0/b/dscbppimt-cms-storage.appspot.com/o/member_images%2Fdiganta_ghosh_Y9EpQ81.jpeg?alt=media&token=1161a4e2-159f-464b-9c95-0f6960b9edc4",
+        linkedin: "https://www.linkedin.com/in/diganta-ghosh-b06a321b9/",
+        github: "https://github.com/diganta413",
+        pos: "Tech Volunteer"
+
+    },
+    {
+        name: "Debjita Mondal",
+        image: "https://firebasestorage.googleapis.com/v0/b/dscbppimt-cms-storage.appspot.com/o/member_images%2Fdebjita_mondal.jpg?alt=media&token=6b96793a-7d90-4eb5-a49f-90e5addf89f9",
+        linkedin: "https://www.linkedin.com/in/debjita-mondal-79663a214/",
+        github: "https://github.com/debjita",
+        pos: "Tech Volunteer",
+
+    },
     {
         name: "Maitrayo Das",
         image: " https://firebasestorage.googleapis.com/v0/b/dscbppimt-cms-storage.appspot.com/o/member_images%2Fmaaitrayo_das_ditNPDZ.webp?alt=media&token=2df0d1ca-9cf8-4f50-ab63-ef2400dd1fc0",
